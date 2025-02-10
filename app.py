095c42caa32e81c5227ac241bc015f1ca78b6f90
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2025-02-10 14:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ language_map = {
 lang_code, output_language = language_map[language]
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro-002",
+    model_name="gemini-2.0-pro-exp-02-05",
     generation_config=generation_config,
     safety_settings=[
         {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
